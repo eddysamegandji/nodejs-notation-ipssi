686393eac1475a26efcd279df5aa9852c4e47cf1
amegandji <eddysamegandji@gmail.com>
2020-01-15 15:58:34 +0100

    merge

--- src/app/api/controllers/userController.js
+++ src/app/api/controllers/userController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const userModel = require('../models/userModel')
 const User = mongoose.model('User');
 
-exports.register = function(req, res) {
+exports.register = (req, res) => {
     var newUser = new User(req.body);
     newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
     newUser.save((err, user) => {
@@ -18,7 +18,7 @@ exports.register = function(req, res) {
     });
 }
 
-exports.sign_in = function(req, res) {
+exports.sign_in = (req, res) => {
     User.findOne({
         email: req.body.email
       }, function(err, user) {
@@ -38,8 +38,50 @@ exports.sign_in = function(req, res) {
       });
 }
 
-exports.loginRequired = function(req, res, next) {
-    if (req.user) {
+exports.delete_user = (req, res) => {
+    User.findOneAndRemove({email: req.body.email}, (err, res) => {
+        if (err) {
+            res.status(500).json({message: "Erreur lors de la supression"});
+        } else {
+            res.status(200).json({success: true})
+        }
+    })
+}
+
+exports.modify_user = (req, res) => {
+    req.body.hash_password = bcrypt.hashSync(req.body.password, 10);
+    User.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
+        if (err) {
+            res.status(500).json({message: "Erreur lors de la modification"});
+        } else {
+            res.status(200).json({success: true});
+        }
+    })
+}
+
+exports.get_all = (req, res) => {
+    User.find((err, user) => {
+        if (err) {
+            res.status(500).json({message: "Erreur lors de la récupération des Users"});
+        } else {
+            for (let index = 0; index < user.length; index++) {
+                user[index].hash_password = undefined;
+            }
+            res.status(200).json(user);
+        }
+    })
+}
+
+exports.adminRequired = function(req, res, next) {
+    if (req.user && user.role == 0) {
+        next();
+      } else {
+        return res.status(401).json({ message: 'Utilisateur non autorisé' });
+      }
+}
+
+exports.studentRequired = function(req, res, next) {
+    if (req.user && user.role == 2) {
         next();
       } else {
         return res.status(401).json({ message: 'Utilisateur non autorisé' });
--- src/app/api/routes/userRoute.js
+++ src/app/api/routes/userRoute.js
@@ -1,4 +1,5 @@
 const user = require('../controllers/userController');
+const userHandlers = require('../controllers/userController.js');
 
 module.exports = (app) => {
     app.route('/auth/register')
@@ -6,4 +7,11 @@ module.exports = (app) => {
 
     app.route('/auth/sign_in')
     .post(user.sign_in);
+
+    app.route('/users')
+    .get(userHandlers.adminRequired, user.get_all);
+    
+    app.route('/users/:id')
+    .put(userHandlers.adminRequired, user.modify_user)
+    .delete(userHandlers.adminRequired, user.delete_user)
 }
