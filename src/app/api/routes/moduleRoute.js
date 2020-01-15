ef9628af4b7e38435827928fcfa3c2c46c1574a5
amegandji <eddysamegandji@gmail.com>
2020-01-15 16:04:45 +0100

    fix route

--- src/app/api/routes/moduleRoute.js
+++ src/app/api/routes/moduleRoute.js
@@ -1,15 +1,16 @@
-const module = require('../controllers/moduleController');
+const moduleController = require('../controllers/moduleController');
+const userHandlers = require('../controllers/userController.js');
 
 module.exports = (app) => {
     app.route('/module/ajout')
-    .post(module.add_module);
+    .post(userhandlers.adminRequired,moduleController.add_module);
 
-    app.route('/module/:intervenant_id/modules')
-    .get(module.list_all_modules);
+    app.route('/module')
+    .get(userhandlers.adminRequired,moduleController.list_all_modules);
     
-    app.route('/module/:module_name/modules')
-    .get(module.get_a_module)
-    .put(module.update_a_module)
-    .delete(module.delete_a_module);
+    app.route('/module/:module_name')
+    .get(userhandlers.adminRequired,moduleController.get_a_module)
+    .put(userhandlers.adminRequired,moduleController.update_a_module)
+    .delete(userhandlers.adminRequired,moduleController.delete_a_module);
 }
 
