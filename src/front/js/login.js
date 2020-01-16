4c2c36f65aebfc86dd98e97fd21a44cdb7374778
amegandji <eddysamegandji@gmail.com>
2020-01-16 12:41:24 +0100

    wip loginpage

---
+++ src/front/js/login.js
@@ -0,0 +1,37 @@
+$().ready(function() {
+    $("#form1").validate({
+        rules : {
+            mail : {
+              required : true
+            },
+            password : {
+              required : true,
+              
+            }
+        },
+          messages : {
+            "mail" : "Veuillez fournir un email correcte",
+            "password" : "Veuillez entrer votre mot de passe"
+            
+          }
+        
+   });
+
+   $('#form1').submit((e) =>{
+     e.preventDefault()
+     var settings = {
+        "url": "localhost:3000/auth/sign_in",
+        "method": "POST",
+        "data": {
+          "email": "$('#mail').val();",
+          "password": "$('#password').val();"
+        }
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+   })
+
+   
+  });
